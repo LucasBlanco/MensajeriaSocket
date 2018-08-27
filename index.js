fa20d579e9d059010db0898678f3dcd7084af026
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2018-08-27 17:47:24 -0300

    asd

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ var getClienteBySocketId =  socketId => clientes.find(elem => elem.socketId == s
 var getClienteByUser = user => clientes.find(elem => elem.user == user)
 
 
-var updateClientes = function(accion, data, userNoANotificar){
+var updateClientes = function(accion, data, userNoANotificar, pantalla){
     clientes.forEach(clienteANotificar => {
         console.log('updating client', clienteANotificar)
-        if(clienteANotificar.user != userNoANotificar){
+        if(clienteANotificar.user != userNoANotificar && clienteANotificar.pantalla == pantalla){
             io.sockets.connected[clienteANotificar.socketId].emit(accion, data);
         }
     });
@@ -41,7 +41,7 @@ var tomarVenta =  function({user: user, idVenta: idVenta, pantalla: pantalla}) {
     if(cliente != undefined){
         cliente.ventaTomada = idVenta
         if(idVenta != undefined){
-            updateClientes('eliminar', idVenta, user)
+            updateClientes('eliminar', idVenta, user, pantalla)
         }
     }
 }
@@ -50,7 +50,7 @@ var liberarVenta = function ({ user: user, idVenta: idVenta , pantalla: pantalla
     var cliente = getClienteByUser(user)
     if (cliente != undefined && cliente.ventaTomada == idVenta) {
         cliente.ventaTomada = null
-        updateClientes('agregar', idVenta, user)
+        updateClientes('agregar', idVenta, user, pantalla)
     }
     
 }
@@ -59,41 +59,44 @@ var eliminarVenta = function ({ user: user, idVenta: idVenta , pantalla: pantall
     var cliente = getClienteByUser(user)
     if (cliente != undefined) {
         cliente.ventaTomada = null
-        updateClientes('eliminar', idVenta, user)
+        //updateClientes('eliminar', idVenta, user, pantalla)
     }
 }
 
-var getVentasTomadas = function(){
+var getVentasTomadas = function(pantalla){
     return clientes.map(cliente =>{
-        if(cliente.ventaTomada != null){ 
+        if(cliente.ventaTomada != null && cliente.pantalla == pantalla){ 
             return cliente.ventaTomada
         }
     })
 }
+
 io.on('connection', function(socket){
     console.log('made socket connection', socket.id)
 
-    socket.on('crearUsuario', function (user) {
+    socket.on('crearUsuario', function (usuario) {
         console.log('socket: ',socket.id)
         var nuevoCliente = new Object();
-        nuevoCliente.user = user;
-        nuevoCliente.pantalla = user.pantalla;
+        nuevoCliente.user = usuario.nombre;
+        nuevoCliente.pantalla = usuario.pantalla;
         nuevoCliente.socketId = socket.id;
         nuevoCliente.ventaTomada = null
         clientes.push(nuevoCliente);
     });
 
-    socket.on('getVentasTomadas', function (data){
-        socket.emit('filtrar', getVentasTomadas());
+    socket.on('getVentasTomadas', function (pantalla){
+        socket.emit('ventasTomadas', getVentasTomadas(pantalla));
     })
 
     socket.on('tomar', function (data) {
         console.log('Socket ID: ', socket.id)
         console.log('User: ', data.user)
-        console.log('Toma venta: ', data.idVenta)
-        if (clientes.some(client => client.ventaTomada == data.idVenta && client.pantalla == data.pantalla)) {
+        console.log('De la pantalla: ', data.pantalla)
+        if (clientes.some(client => client.ventaTomada == data.idVenta && data.user != client.user && client.pantalla == data.pantalla)) {
             socket.emit('ventaTomada', 'ventaTomada');
+            console.log('Se le deniega la venta porque ya esta tomada: ')
         }else{
+            console.log('Toma venta: ', data.idVenta)
             tomarVenta(data)
         }
     });
@@ -102,6 +105,7 @@ io.on('connection', function(socket){
         console.log('Socket ID: ', socket.id)
         console.log('User: ', data.user)
         console.log('Libera venta: ', data.idVenta)
+        console.log('De la pantalla: ', data.pantalla)
         liberarVenta(data)
         
     });
@@ -110,14 +114,17 @@ io.on('connection', function(socket){
         console.log('Socket ID: ', socket.id)
         console.log('User: ', data.user)
         console.log('Elimina venta: ', data.idVenta)
+        console.log('De la pantalla: ', data.pantalla)
         eliminarVenta(data)
-        
     });
 
     socket.on('disconnect', function () {
         var cliente = getClienteBySocketId(socket.id)
         var index = clientes.indexOf(cliente)
         if (index > -1) {
+            if(cliente.pantalla){
+                updateClientes('agregar', cliente.ventaTomada, cliente.user, cliente.pantalla)
+            }
             clientes.splice(index, 1);
             console.log('client disconnected', cliente.user)
         }
